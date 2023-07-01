8023cafa88f0776e70e1b800582090f50b31bfcc
Suyel Haque <slsuyel@gmail.com>
2023-07-01 23:36:40 +0600

    base url change

--- src/baseUrl/baseUrl.jsx
+++ src/baseUrl/baseUrl.jsx
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://ecommerce-server-bay.vercel.app';
--- src/pages/Shared/StickyNavbar/StickyNavbar.jsx
+++ src/pages/Shared/StickyNavbar/StickyNavbar.jsx
@@ -4,32 +4,32 @@ const StickyNavbar = () => {
     return (
         <div className="sticky-navbar">
             <div className="sticky-info">
-                <Link to="">
+                <Link to="/">
 
                     <img src="https://www.svgrepo.com/show/507339/home-alt.svg" alt="" width={'20px'} />
                     Home
                 </Link>
             </div>
             <div className="sticky-info">
-                <Link to="demo1-shop.html" className="">
+                <Link to="/categories/Electronics" className="text-decoration-none">
                     <img src="https://www.svgrepo.com/show/497852/category.svg" alt="" width={'20px'} />
                     Categories
                 </Link>
             </div>
             <div className="sticky-info">
-                <Link to="wishlist.html" className="">
+                <Link to="/" className="text-decoration-none">
                     <img src="https://www.svgrepo.com/show/512449/love-1488.svg" alt="" width={'20px'} />
                     Wishlist
                 </Link>
             </div>
             <div className="sticky-info">
-                <Link to="login.html" className="">
+                <Link to="/dashboard" className="text-decoration-none">
                     <img src="https://www.svgrepo.com/show/514279/user-circle.svg" alt="" width={'20px'} />
                     Account
                 </Link>
             </div>
             <div className="sticky-info">
-                <Link to="cart.html" className="">
+                <Link to="/cart" className="text-decoration-none">
                     <img src="https://www.svgrepo.com/show/509786/cart.svg" alt="" width={'20px'} />
                     Cart
                 </Link>
